0a914dfeea94804857bcddacce40ce2331b0d3c7
dastasio <davide.stasio@gmail.com>
2016-11-20 17:38:25 +0100

    Started

--- lib/pointGen.cpp
+++ lib/pointGen.cpp
@@ -3,66 +3,52 @@
 const int times_to_subdivide = 5;
 const int num_triangles = 729; // 3^5 triangles
 const int num_vertices = 3 * num_triangles;
-vec2 points[num_vertices];
-int Index = 0;
-
-void init() {
-	vec2 v[3] = { 
-		vec2(-1.0, -1.0), vec2( 0.0, 1.0), vec2( 1.0, -1.0)
-	};
-	divide_triangle( v[0], v[1], v[2], times_to_subdivide);
-
-
-	GLuint program = InitShader( "vshader.glsl", "fshader.glsl");
-	glUseProgram( program);
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao); /* get identifier */
-	glBindVertexArray( vao);
-
-	
-	GLuint buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer( GL_ARRAY_BUFFER, buffer); /* creates buffer with given id */
-	glBufferData( GL_ARRAY_BUFFER, sizeof( points), points, GL_STATIC_DRAW);
-				/* allocates memory */
-
-	GLuint loc = glGetAttribLocation( program, "vPosition");
-	glEnableVertexAttribArray( loc);
-	glVertexAttribPointer( loc, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0) );
-
-	glClearColor( 0.0, 0.05, 0.1, 1.0); 
-
-	display();
-}
-
+vec3 points[num_vertices];
 
 // ------------------------------------------------------------------------------
 //! specifies vertices for a triangle
-void triangle( const vec2 &a, const vec2 &b,const vec2 &c) {
+void triangle( const vec3 &a, const vec3 &b,const vec3 &c) {
+	static int Index = 0;
 	points[ Index++] = a;
 	points[ Index++] = b;
 	points[ Index++] = c;
 }
 
-// ------------------------------------------------------------------------------
-//! divides given triangle if k != 0
-void divide_triangle(const vec2 &a, const vec2  &b,const vec2 &c, int k) {
-	if( k > 0) {
-		// calculate midpoints of sides
-		point2 v0 = ( a + b)/2.0;
-		point2 v1 = ( a + c)/2.0;
-		point2 v2 = ( b + c)/2.0;
-		// subdivide all triangles but inner
-		divide_triangle( a, v0, v1, k-1);
-		divide_triangle( c, v1, v2, k-1);
-		divide_triangle( b, v2, v0, k-1);
-	}
-	else {
-		triangle( a, b, c); /* draw triangle at end of recursion */
-	}
+// -------------------------------------------------------------------------------
+//! draws a tetrahedron
+
+void tetra( vec3 a, vec3 b, vec3 c, vec3 d) {
+	triangle( a, b, c);
+	triangle( a, c, d);
+	triangle( a, d, b);
+	triangle( b, d, c);
 }
 
+// ------------------------------------------------------------------------------
+//! divides a tetrahedron
+
+void divide_tetra( vec3 a, vec3 b, vec3 c, vec3 d, int m) {
+	if( m > 0) {
+		vec3 mid[6];
+
+		// compute midpoints
+
+		mid[0] = (a + b)/2.0;
+		mid[1] = (a + c)/2.0;
+		mid[2] = (a + d)/2.0;
+		mid[3] = (b + c)/2.0;
+		mid[4] = (c + d)/2.0;
+		mid[5] = (b + d)/2.0;
+
+		// create tetrahedra
+
+		divide_tetra( a, mid[0], mid[1], mid[2], m-1);
+		divide_tetra( mid[0], b, mid[3], mid[5], m-1);
+		divide_tetra( mid[1], mid[3], c, mid[4], m-1);
+		divide_tetra( mid[2], mid[5], mid[5], d, m-1);
+	}
+	else tetra( a, b, c, d);
+}
 
 // -------------------------------------------------------------------------------
 //! renders image
