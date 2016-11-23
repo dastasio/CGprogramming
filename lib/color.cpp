2c7a280a68fbf4bc6b4399849822d46271a1560e
dastasio <davide.stasio@gmail.com>
2016-11-23 17:00:10 +0100

    Fixed bugs -- WORKING

--- lib/color.cpp
+++ lib/color.cpp
@@ -4,7 +4,7 @@ void init() {
 	
 	vec2 v[3] = {	vec2( -0.75, -0.75),
 					vec2(  0.75, -0.75),
-					vec2(  0.75,  0.75) };
+					vec2(  0.0,  0.75) };
 	
 	vec3 c[3] = {	vec3( 1.0, 0.0, 0.0),
 					vec3( 0.0, 1.0, 0.0),
@@ -27,9 +27,11 @@ void init() {
 
 	GLuint loc_vertex, loc_color;
 	loc_vertex = glGetAttribLocation( program, "vPosition");
+	glEnableVertexAttribArray( loc_vertex);
 	glVertexAttribPointer( loc_vertex, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
 
 	loc_color = glGetAttribLocation( program, "vColor");
+	glEnableVertexAttribArray( loc_color);
 	glVertexAttribPointer( loc_color, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET( sizeof(v)));
 
 }
